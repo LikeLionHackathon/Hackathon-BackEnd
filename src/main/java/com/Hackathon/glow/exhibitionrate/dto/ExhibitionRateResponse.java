984ebaa2b11820bf41bdf6d6cb1f95c02c379569
박서영 <emilypark0721@gmail.com>
2025-08-22 19:28:37 +0900

    전시 평가 수정 완료(포스트맨 완료)

--- src/main/java/com/Hackathon/glow/exhibitionrate/dto/ExhibitionRateResponse.java
+++ src/main/java/com/Hackathon/glow/exhibitionrate/dto/ExhibitionRateResponse.java
@@ -11,14 +11,15 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 public class ExhibitionRateResponse {
-    private Long id;
-    private Long rate;
+    private Long exhibitionrateId;
+
     private Long userId;      // 작성자 ID
     private Long exhibitionId;
 
+    private Long rate;
     public static ExhibitionRateResponse from(ExhibitionRate rate) {
         return ExhibitionRateResponse.builder()
-                .id(rate.getId())
+                .exhibitionrateId(rate.getId())
                 .rate(rate.getRate())
                 .userId(rate.getUser() != null ? rate.getUser().getUserId() : null)
                 .exhibitionId(rate.getExhibition() != null ? rate.getExhibition().getId() : null)
